cdd69c39c60924eef9f50bb0d13c9a4c172b9a30
19Hamid91 <99725605+19Hamid91@users.noreply.github.com>
2023-01-10 09:43:05 +0700

    make story

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text nameText;
+	public Text dialogueText;
+
+	public Animator animator;
+	public Animator animator2;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+	}
+
+	public void StartDialogue (Dialogue dialogue)
+	{
+		animator.SetBool("IsOpen", true);
+		animator2.SetBool("PlayerIsOpen", true);
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence ()
+	{
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return new WaitForSeconds(0.01F);
+		}
+	}
+
+	void EndDialogue()
+	{
+		animator.SetBool("IsOpen", false);
+        animator2.SetBool("PlayerIsOpen", false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
